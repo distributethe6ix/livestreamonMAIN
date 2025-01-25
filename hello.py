129ee79df145b9183a8f7e76bbf189ab5b900e46
Marino Wijay <marino.wijay@gmail.com>
2025-01-25 07:45:53 -0500

    update with bedrock

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
-name = "Marino"
-location = "Toronto"
 
-print("Hello",name, "from", location)
+name = input("What is your name? ")
+location = input("Where are you from? ")
+
+if name == "Marino":
+
+    print("Howdy, Marino! How's it going in" + location + "?")
+else:   
+    print("Hello, " + name + "! How's it going in " + location + "?")
+
+for i in name:
+    print(i)
